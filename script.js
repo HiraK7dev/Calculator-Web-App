bde237a6931a84372fa9fa299c0edb1be3b8dec8
Hirak <hirak.bhatta2004@gmail.com>
2024-02-17 09:48:03 +0530

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const signs = [`C`, `%`, `CE`, `/`, `7`, `8`, `9`, `*`, `4`, `5`, `6`, `-`, `1`, `2`, `3`, `+`, `00`, `0`, `.`, `=`];
+const signs = [`C`, `(`, `)`, `/`, `7`, `8`, `9`, `*`, `4`, `5`, `6`, `-`, `1`, `2`, `3`, `+`, `00`, `0`, `.`, `=`];
 const functionalSigns = [];
 const btn = [];
 
@@ -7,20 +7,22 @@ let signCounter = 0;
 const mainDiv = document.querySelector("#main");
 const rows = document.querySelectorAll(".row");
 const screen = document.querySelector("#screen");
+const errorHandler = document.querySelector("#errorHandler");
 
 const resultChecker = () => {
-    screen.value = eval(screen.value);
+    try {
+        screen.value = eval(screen.value);
+        errorHandler.innerText = ``;
+    } catch (error) {
+        errorHandler.innerText = `ERROR`;
+    }
 }
 
 for(let f=0; f<=20; f++){
     if(signs[f] === `C`){
         functionalSigns[f] = () => {
             screen.value = ``;
-        }
-    }
-    else if(signs[f] === `CE`){
-        functionalSigns[f] = () => {
-            screen.value = ``;
+            errorHandler.innerText = ``;
         }
     }
     else if(signs[f] === `=`){
